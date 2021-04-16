64a1fcae30c0245644cf8a0e23762da3dea724ef
Lucas Vieira <lvieirastos@gmail.com>
2021-04-16 17:19:13 -0300

    criado e aperfeiçoado LoginActivity.java

--- app/src/main/java/com/lucasvieira/servicocerto/LoginActivity.java
+++ app/src/main/java/com/lucasvieira/servicocerto/LoginActivity.java
@@ -10,7 +10,7 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private int cont = 0, entrar = 0;
+    private int cont = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,21 +21,17 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void logarUsuario(View view){
-        Toast.makeText(this, "Verifique seu usuário/senha", Toast.LENGTH_SHORT).show();
-        cont++;
 
         if (cont == 3){
-            Toast.makeText(this, "ESQUECE, TU NÃO VAI ENTRAR", Toast.LENGTH_SHORT).show();
-            cont = 0;
-            entrar++;
-
-            if (entrar == 3) {
-                Toast.makeText(this, "Entra aí vai...", Toast.LENGTH_SHORT).show();
-                entrar = 0;
+                Toast.makeText(this, "Entra aí vai...", Toast.LENGTH_LONG).show();
 
                 Intent i = new Intent(this, MainActivity.class);
                 startActivity(i);
-            }
+
+                cont = 0;
         }
+
+        Toast.makeText(this, "Verifique seu usuário/senha", Toast.LENGTH_SHORT).show();
+        cont++;
     }
 }
