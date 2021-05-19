50af70cb06c73d3f3a231ebea5593b9a5a32725e
Lucas Vieira <lvieirastos@gmail.com>
2021-05-19 08:48:48 -0300

    Navigation Fragments

--- app/src/main/java/com/lucasvieira/servicocerto/activity/MainActivity.java
+++ app/src/main/java/com/lucasvieira/servicocerto/activity/MainActivity.java
@@ -44,11 +44,12 @@ public class MainActivity extends AppCompatActivity {
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_servico, R.id.nav_notificacoes, R.id.nav_chat, R.id.nav_favoritos, R.id.nav_conta, R.id.nav_ajuda, R.id.nav_termos)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavController navController = Navigation.findNavController(this, R.id.nav_home);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
@@ -62,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavController navController = Navigation.findNavController(this, R.id.nav_home);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
